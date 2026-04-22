3a643ed0b4e3a6efb2e8b3b4396c64eb54342738
Marco Ferreira <marcoferreira@MacBook-Pro-de-Marco-2.local>
2026-04-22 05:11:12 +0200

    feat: add Élévations frontales haltères video + thumbnail

---
+++ supabase/migrations/20260422_elevations_frontales_halteres_video.sql
@@ -0,0 +1,4 @@
+UPDATE exercises_db SET
+  video_url = '/videos/exercises/elevations-frontales-halteres.mp4?v=4',
+  gif_url = '/videos/exercises/elevations-frontales-halteres.jpg?v=4'
+WHERE name = 'Élévations frontales haltères';
